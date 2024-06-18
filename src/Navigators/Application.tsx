f8c055fab4ae48bf6b0cb1dc8f2a6d787efdd742
dipak-tomar <dipakkumartomar758@gmail.com>
2024-06-18 10:48:53 +0530

    added native base ,navigation and firebase in ios

---
+++ src/Navigators/Application.tsx
@@ -0,0 +1,51 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {Platform, StatusBar} from 'react-native';
+import {navigationRef} from './utils';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {Box} from 'native-base';
+import Login from '../screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+const ApplicationNavigator = () => {
+  const routeNameRef = React.useRef();
+
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        //   SplashScreen.hide();
+        // @ts-ignore
+        routeNameRef.current = navigationRef.current.getCurrentRoute().name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+
+        // @ts-ignore
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+        }
+        // @ts-ignore
+        routeNameRef.current = currentRouteName;
+        console.log('current screen', currentRouteName);
+      }}>
+      <StatusBar
+        barStyle={'dark-content'}
+        networkActivityIndicatorVisible={true}
+        backgroundColor={'white'}
+      />
+
+      <Stack.Navigator
+        initialRouteName={'Login'}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default ApplicationNavigator;
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import {Box, Text} from 'native-base';
+
+const Login = () => {
+  return (
+    <Box safeArea>
+      <Text>login</Text>
+    </Box>
+  );
+};
+
+export default Login;
